77c7c8e73c80d874801fb6c2e9e5cb7a263288b9
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-02-28 23:57:11 +0600

    finish card UI

--- js/mobile.js
+++ js/mobile.js
@@ -9,7 +9,23 @@ const searchMobile = () =>{
 }
 const showSearchResult = mobiles =>{
     // console.log(mobiles);
+    const showResultDivId = document.getElementById('showResults');
     mobiles.forEach(mobile=>{
+        const childDiv = document.createElement('div');
+        childDiv.classList.add('col', 'text-center');
+        childDiv.innerHTML = `
+        <div class="card align-items-center p-3 border-secondary">
+            <img src="${mobile.image}" class="card-img-top w-50 " alt="...">
+            <div class="card-body">
+                <h2 class="card-title">${mobile.phone_name}</h2>
+                <h3 class ="card-title">${mobile.brand}</h3>
+                <div class="card-footer border-0 bg-transparent">
+                    <button class="btn btn-primary">Explore</button>
+                </div>
+            </div>
+        </div>
+        `;
+        showResultDivId.appendChild(childDiv);
         console.log(mobile);
     });
 
