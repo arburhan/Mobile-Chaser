dcfbfb34bde1a164b40f33364cf9f2e4456979ab
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-02-28 23:00:30 +0600

    finish basic structure

--- js/mobile.js
+++ js/mobile.js
@@ -1,4 +1,17 @@
 // search function *arrow
-const searchMobile = ()=>{
-    console.log('mobile areana');
+const searchMobile = () =>{
+    const searchFieldId = document.getElementById('search-field');
+    const searchText = searchFieldId.value;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => showSearchResult(data.data));
+}
+const showSearchResult = mobiles =>{
+    // console.log(mobiles);
+    mobiles.forEach(mobile=>{
+        console.log(mobile);
+    });
+
+    //
 }
