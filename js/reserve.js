bf0e35b367b863340b772c0a5ca69d9b739cd0ff
Md Borhan Uddin Ashik <burhan128454@outlook.com>
2022-03-02 13:23:00 +0600

    release date condition finish

--- js/reserve.js
+++ js/reserve.js
@@ -1,10 +1,14 @@
-// single value id
+/* // single value id
 const singleDivId = document.getElementById('showSingle');
 // show result id
 const showResultDivId = document.getElementById('showResults');
 // error message id 
 const errorMsg = document.getElementById('errorMsg');
-// search function *arrow
+// results 20 see-more button & div
+const seeMoreButtonId = document.getElementById('seeMoreButtonId');
+const result20Div = document.getElementById('showResults20');
+
+// search function 
 const searchMobile = () =>{
     singleDivId.textContent = '';
     const searchFieldId = document.getElementById('search-field');
@@ -33,7 +37,6 @@ const displayError=(error)=>{
 }
 // search section
 const showSearchResult = mobiles =>{
-    console.log(mobiles.length);
     showResultDivId.textContent = '';
     // single value display none
     singleDivId.style.display='none';
@@ -44,11 +47,41 @@ const showSearchResult = mobiles =>{
             <img class="img-fluid" src="./images/resultNotFound.gif">
         `;
     }
+
    else{
-       console.log(mobiles);
-       if(mobiles.length >21){
-           console.log(654);
-       }
+            seeMoreButtonId.innerHTML=`
+            <button onclick="loadAll(${mobiles})" class="rounded-pill mx-auto bg-primary border-0 fs-5" id="see-more">See More</button>
+            `;
+            if(mobiles.length >21){
+                const value20 = mobiles.slice(0, 20);
+                value20.forEach(mobile20=>{
+                    const childDiv20 = document.createElement('div');
+                    childDiv20.classList.add('col', 'text-center');
+                    childDiv20.innerHTML = `
+                        <div class="card align-items-center p-3 border-secondary">
+                            <img src="${mobile20.image}" class="card-img-top w-50 " alt="...">
+                            <div class="card-body">
+                                <h2 class="card-title">${mobile20.phone_name}</h2>
+                                <h3 class ="card-title">${mobile20.brand}</h3>
+                                <div class="card-footer border-0 bg-transparent">
+                                    <button onclick="loadDetails('${mobile20.slug}')" class="btn btn-primary">Explore</button>
+                                </div>
+                            </div>
+                        </div>
+                    `;  
+                    result20Div.appendChild(childDiv20);
+                });
+                    seeMoreButtonId.style.display='block';
+            } 
+    }  
+}
+// see more button
+const loadAll=mobiles=>{
+    console.log(mobiles);
+    // seeMoreButtonId.style.display='none';
+    result20Div.style.display='none';
+    // showResults.style.display='block';
+    // show all
     mobiles.forEach(mobile=>{
         const childDiv = document.createElement('div');
         childDiv.classList.add('col', 'text-center');
@@ -66,25 +99,23 @@ const showSearchResult = mobiles =>{
         `;
         showResultDivId.appendChild(childDiv);
     });
-   }
 }
+
 // load details
 const loadDetails=ID=>{
     const url = `https://openapi.programming-hero.com/api/phone/${ID}`;
     fetch(url)
     .then(res=>res.json())
     .then(data=>showSingle(data.data));
-    
 }
 const showSingle=mobileId=>{
     showResultDivId.textContent = '';
-    console.log(mobileId);
+    // console.log(mobileId);
     // hide search all result
     // showResultDivId.style.display='none';
     singleDivId.style.display='block';
     // sensor details
     const sensorArray = mobileId.mainFeatures.sensors;
-    const releaseDateNew = "Not found in our record";
     // new div
     const firstDiv = document.createElement('div');
     firstDiv.classList.add('px-md-5');
@@ -95,7 +126,7 @@ const showSingle=mobileId=>{
             <h3 class ="card-text"><img class="feature-images" src="./images/brand-image.png">${mobileId.brand}</h3>
             <!-- Main Features -->
             <h4 class="card-text text-success fw-bold">Main Features</h4>
-            <p class ="card-tex">Release Date: ${mobileId.releaseDate? mobileId.releaseDate:releaseDateNew}</p>
+            <p class ="card-tex">${mobileId.releaseDate}</p>
             <p class ="card-tex"><img class="feature-images" src="./images/cpu.png">${mobileId.mainFeatures.chipSet}</p>
             <p class ="card-tex"><img class="feature-images" src="./images/display.png">${mobileId.mainFeatures.displaySize}</p>
             <p class ="card-tex"><img class="feature-images" src="./images/memory-card.png">${mobileId.mainFeatures.storage}</p>
@@ -124,6 +155,39 @@ const showSingle=mobileId=>{
         `;
     }
     singleDivId.appendChild(secondDiv);
-    console.log(mobileId);
+    // console.log(mobileId);
 }
 
+
+
+
+// reserve
+
+const showSingle=mobileId=>{
+    showResultDivId.textContent = '';
+    console.log(mobileId);
+    // hide search all result
+    // showResultDivId.style.display='none';
+    singleDivId.style.display='block';
+    // sensor details
+    const sensorArray = mobileId.mainFeatures.sensors;
+    // new div
+    const firstDiv = document.createElement('div');
+    const releaseDateNew = "Not found in our record";
+    firstDiv.classList.add('px-md-5');
+    firstDiv.innerHTML=`
+        <img src="${mobileId.image}" class="card-img-top w-25 img-fluid" alt="...">
+        <div class="card-body">
+            <h2 class="card-title">${mobileId.name}</h2>
+            <h3 class ="card-text"><img class="feature-images" src="./images/brand-image.png">${mobileId.brand}</h3>
+            <!-- Main Features -->
+            <h4 class="card-text text-success fw-bold">Main Features</h4>
+            <p class ="card-tex">Release Date: ${mobileId.releaseDate? mobileId.releaseDate:releaseDateNew}</p>
+            <p class ="card-tex"><img class="feature-images" src="./images/cpu.png">${mobileId.mainFeatures.chipSet}</p>
+            <p class ="card-tex"><img class="feature-images" src="./images/display.png">${mobileId.mainFeatures.displaySize}</p>
+            <p class ="card-tex"><img class="feature-images" src="./images/memory-card.png">${mobileId.mainFeatures.storage}</p>
+            <p id="sensorId" class="card-text"><span class="other-feature"> Sensors: </span>${sensorArray.join(', ')}</p>
+            <div class="card-footer border-0 bg-transparent">
+            </div>
+        </div>
+    `; */
